726c13bff26631a3d0356d031e6fce04a1626723
Franck Aimé Koukougnon <48843707+franckivoire@users.noreply.github.com>
2019-03-27 11:56:24 +0100

    Add files via upload
    fais chier en fait

--- Simon/assets/assets/scriptSimon.js
+++ Simon/assets/assets/scriptSimon.js
@@ -1,6 +1,21 @@
 
-$ (document).ready(function(){
-$ 
+$(document).ready(function(){
+    $('#go').click(function(){
+        console.log("test");
+    });
+    $('#green').click(function(){
+        console.log("testGreen");
+    });
+    $('#red').click(function(){
+        console.log("testRed");
+    });
+    $('#yellow').click(function(){
+        console.log("testYellow");
+    });
+    $('#blue').click(function(){
+        console.log("testBlue");
+    });
 
 
-})
+
+});
